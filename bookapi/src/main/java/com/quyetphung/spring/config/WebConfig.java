6d9734ded87a5351656c7e9c5310ecc4a4ead32f
phungvanquyet94 <phungvanquyet94@gmail.com>
2019-02-09 15:39:31 +0700

    ceate WebConfig

---
+++ bookapi/src/main/java/com/quyetphung/spring/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.quyetphung.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.quyetphung.spring.controller"})
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+	
+	
+}
